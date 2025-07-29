Fix redundant scaling in F.scaled_dot_product_attention

configs/configs_base.py
@@ -123,7 +123,7 @@ model_configs = {
         1
     ),  # NOTE: Number of blocks in each activation checkpoint, if None, no checkpointing is performed.
     # switch of kernels
-    "use_memory_efficient_kernel": False,
+    "use_memory_efficient_kernel": False,  # whether to use the torch.nn.functional.scaled_dot_product_attention, Defaults to False.
     "use_deepspeed_evo_attention": True,
     "use_flash": False,
     "use_lma": False,
@@ -239,6 +239,9 @@ model_configs = {
         },
         "diffusion_module": {
             "use_fine_grained_checkpoint": True,
+            "use_efficient_implementation": GlobalConfigValue(
+                "use_memory_efficient_kernel"
+            ),
             "sigma_data": GlobalConfigValue("sigma_data"),
             "c_token": 768,
             "c_atom": GlobalConfigValue("c_atom"),

protenix/model/protenix.py
@@ -74,7 +74,9 @@ class Protenix(nn.Module):
 
         # Model
         esm_configs = configs.get("esm", {})  # This is used in InputFeatureEmbedder
-        self.input_embedder = InputFeatureEmbedder(**configs.model.input_embedder, esm_configs=esm_configs)
+        self.input_embedder = InputFeatureEmbedder(
+            **configs.model.input_embedder, esm_configs=esm_configs
+        )
         self.relative_position_encoding = RelativePositionEncoding(
             **configs.model.relative_position_encoding
         )
@@ -200,7 +202,6 @@ class Protenix(nn.Module):
                         z += self.template_embedder(
                             input_feature_dict,
                             z,
-                            use_memory_efficient_kernel=self.configs.use_memory_efficient_kernel,
                             use_deepspeed_evo_attention=self.configs.use_deepspeed_evo_attention
                             and deepspeed_evo_attention_condition_satisfy,
                             use_lma=self.configs.use_lma,
@@ -212,7 +213,6 @@ class Protenix(nn.Module):
                         z,
                         s_inputs,
                         pair_mask=None,
-                        use_memory_efficient_kernel=self.configs.use_memory_efficient_kernel,
                         use_deepspeed_evo_attention=self.configs.use_deepspeed_evo_attention
                         and deepspeed_evo_attention_condition_satisfy,
                         use_lma=self.configs.use_lma,
@@ -224,7 +224,6 @@ class Protenix(nn.Module):
                         z = z + self.template_embedder(
                             input_feature_dict,
                             z,
-                            use_memory_efficient_kernel=self.configs.use_memory_efficient_kernel,
                             use_deepspeed_evo_attention=self.configs.use_deepspeed_evo_attention
                             and deepspeed_evo_attention_condition_satisfy,
                             use_lma=self.configs.use_lma,
@@ -236,7 +235,6 @@ class Protenix(nn.Module):
                         z,
                         s_inputs,
                         pair_mask=None,
-                        use_memory_efficient_kernel=self.configs.use_memory_efficient_kernel,
                         use_deepspeed_evo_attention=self.configs.use_deepspeed_evo_attention
                         and deepspeed_evo_attention_condition_satisfy,
                         use_lma=self.configs.use_lma,
@@ -248,7 +246,6 @@ class Protenix(nn.Module):
                     s,
                     z,
                     pair_mask=None,
-                    use_memory_efficient_kernel=self.configs.use_memory_efficient_kernel,
                     use_deepspeed_evo_attention=self.configs.use_deepspeed_evo_attention
                     and deepspeed_evo_attention_condition_satisfy,
                     use_lma=self.configs.use_lma,
@@ -467,7 +464,6 @@ class Protenix(nn.Module):
             z_trunk=z,
             pair_mask=None,
             x_pred_coords=pred_dict["coordinate"],
-            use_memory_efficient_kernel=self.configs.use_memory_efficient_kernel,
             use_deepspeed_evo_attention=self.configs.use_deepspeed_evo_attention
             and deepspeed_evo_attention_condition_satisfy,
             use_lma=self.configs.use_lma,
@@ -615,7 +611,6 @@ class Protenix(nn.Module):
             pair_mask=None,
             x_pred_coords=coordinate_mini,
             use_embedding=not drop_embedding,
-            use_memory_efficient_kernel=self.configs.use_memory_efficient_kernel,
             use_deepspeed_evo_attention=self.configs.use_deepspeed_evo_attention
             and deepspeed_evo_attention_condition_satisfy,
             use_lma=self.configs.use_lma,

protenix/openfold_local/model/primitives.py
@@ -502,7 +502,6 @@ class Attention(nn.Module):
         q_x: torch.Tensor,
         kv_x: torch.Tensor,
         biases: Optional[List[torch.Tensor]] = None,
-        use_memory_efficient_kernel: bool = False,
         use_deepspeed_evo_attention: bool = False,
         use_lma: bool = False,
         lma_q_chunk_size: int = DEFAULT_LMA_Q_CHUNK_SIZE,
@@ -518,11 +517,6 @@ class Attention(nn.Module):
                 [*, K, C_k] key data
             biases:
                 List of biases that broadcast to [*, H, Q, K]
-            use_memory_efficient_kernel:
-                Whether to use a custom memory-efficient attention kernel.
-                This should be the default choice for most. If none of the
-                "use_<...>" flags are True, a stock PyTorch implementation
-                is used instead
             use_deepspeed_evo_attention:
                 Whether to use DeepSpeed memory-efficient attention kernel.
                 If none of the "use_<...>" flags are True, a stock PyTorch
@@ -551,7 +545,6 @@ class Attention(nn.Module):
             )
 
         attn_options = [
-            use_memory_efficient_kernel,
             use_deepspeed_evo_attention,
             use_lma,
             use_flash,
@@ -565,12 +558,7 @@ class Attention(nn.Module):
         # DeepSpeed attention kernel applies scaling internally
         q, k, v = self._prep_qkv(q_x, kv_x, apply_scale=not use_deepspeed_evo_attention)
 
-        if is_fp16_enabled():
-            use_memory_efficient_kernel = False
-
-        if use_memory_efficient_kernel:
-            raise Exception(f"use_memory_efficient_kernel=True not supported!!!")
-        elif use_deepspeed_evo_attention:
+        if use_deepspeed_evo_attention:
             if len(biases) > 2:
                 raise ValueError(
                     "If use_deepspeed_evo_attention is True, you may only "