a63676c19736b38b43873798fabcd32936fc6476
abhinav22 <abhinav22@tamu.edu>
2025-08-08 17:09:46 -0500

    made input files

--- hpc/create_input_json.py
+++ hpc/create_input_json.py
@@ -1,16 +1,17 @@
 import os
 import json
+import warnings
 from dataclasses import dataclass
 
-from typing import List
+from typing import List, Tuple
 import biotite.sequence.io.fasta as fasta
 
 
-INPUT_JSON_BASE_PATH = "/home/abhinav22/Documents/holofold_data/fastas/unique_uniprot_results"
-OUTPUT_JSON_BASE_PATH = "/home/abhinav22/Documents/Protenix/run_files"
+INPUT_JSON_BASE_PATH = "/home/abhinav22/Documents/data/holofold_data/unique_uniprot_results"
+OUTPUT_JSON_BASE_PATH = "/home/abhinav22/Documents/dimi-protenix/Protenix/run_files"
 
-FASTA_BASE_PATH = ""
-MSA_BASE_PATH = ""
+FASTA_BASE_PATH = "/home/abhinav22/Documents/data/holofold_data/fastas/all_uniprot_metals"
+MSA_BASE_PATH = "/home/abhinav22/Documents/data/holofold_data/msas/all_uniprot_metals"
 
 @dataclass
 class InputDict:
@@ -39,36 +40,53 @@ class InputDict:
         }
 
 
-def create_json_outputs(ligand, proteins_per_batch: int = 100) -> List[List[dict]]:
+def create_json_outputs(ligand, proteins_per_batch: int = 100) -> Tuple[List[List[dict]], List[str]]:
+    """Returns tuple of json data in batches and a list of the proteins skipped"""
+
     with open(os.path.join(INPUT_JSON_BASE_PATH, f"{ligand}.json"), "r") as f:
         proteins = json.load(f)
 
+    skipped_proteins = []
     final_list: List[List[dict]] = []
     for i in range(len(proteins) // proteins_per_batch + 1):
         batch = []
         for j in range(proteins_per_batch):
+            if j + i * proteins_per_batch >= len(proteins):
+                break
             protein = proteins[j + i * proteins_per_batch]
             fasta_path = os.path.join(FASTA_BASE_PATH, f"{protein}.fasta")
-            assert os.path.exists(fasta_path), f"FASTA file not found: {fasta_path}"
+            if not os.path.exists(fasta_path):
+               warnings.warn(f"FASTA file not found: {fasta_path}")
+               skipped_proteins.append(protein)
+               continue
+
             fasta_file = fasta.FastaFile.read(fasta_path)
             sequence = str(list(fasta_file.values())[0])
 
-            msa_path = os.path.join(MSA_BASE_PATH, f"{protein}.msa")
-            assert os.path.exists(msa_path), f"MSA file not found: {msa_path}"
+            msa_path = os.path.join(MSA_BASE_PATH, f"{protein}")
+            if not os.path.exists(msa_path):
+                warnings.warn(f"MSA file not found: {msa_path}")
+                skipped_proteins.append(protein)
+                continue
 
             batch.append(InputDict(sequence, msa_path, ligand).to_dict())
 
         final_list.append(batch)
-    return final_list
+    return final_list, skipped_proteins
 
 
 if __name__ == "__main__":
 
-    ligands = ["fe"]
+    ligands = ["ca", "co", "k", "mg", "mn", "ni", "zn"]
     for ligand in ligands:
-        output_file_contents = create_json_outputs(ligand)
+        output_file_contents, errors = create_json_outputs(ligand, 100)
+
+        os.makedirs(os.path.join(OUTPUT_JSON_BASE_PATH, ligand), exist_ok=True)
+
+        with open(os.path.join(OUTPUT_JSON_BASE_PATH, ligand, f"{ligand}_errors.json"), "w") as f:
+            json.dump(errors, f, indent=4)
+
         for i, batch in enumerate(output_file_contents):
             output_file_path = os.path.join(OUTPUT_JSON_BASE_PATH, ligand, f"{ligand}_{i}.json")
-            os.makedirs(os.path.join(OUTPUT_JSON_BASE_PATH, ligand), exist_ok=True)
             with open(output_file_path, "w") as f:
                 json.dump(batch, f, indent=4)
